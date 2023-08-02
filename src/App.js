25ca1d927633afbc0e6533dde4970957ed566fa3
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-02 02:36:17 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>YOU ARE CURRENTLY LOOKING AT :</h1>
+      <h1>
+        ROOT/SRC/App.js >
+        <br /> App function >
+        <br /> className="App" div
+      </h1>
+      <h2></h2>
+    </div>
+  );
+}
